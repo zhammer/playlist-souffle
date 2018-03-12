76f52976862b7b87b26c150bd14cf407be3479e7
zhammer <zachary_hammer@brown.edu>
2018-03-11 23:17:58 -0400

    Add scripts to access service endpoints via curl

---
+++ scripts/accesstoken.sh
@@ -0,0 +1,1 @@
+curl -H "Authorization: Bearer $1" https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/accesstoken -XPOST
---
+++ scripts/refreshtoken.sh
@@ -0,0 +1,1 @@
+curl -H "Authorization: Bearer $1" -d redirectUri=https%3A%2F%2F127.0.0.1%3A8100 https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/refreshtoken
