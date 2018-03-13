7f44d7ce3c78f087537f19aaf033af47215d3b9d
zhammer <zachary_hammer@brown.edu>
2018-03-12 21:22:50 -0400

    Add documentation to endpoint scripts

--- scripts/accesstoken.sh
+++ scripts/accesstoken.sh
@@ -1,1 +1,16 @@
-curl -H "Authorization: Bearer $1" https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/accesstoken -XPOST
+#!/usr/bin/env bash
+#
+# Send a request to the /accesstoken endpoint to obtain a new access token from a valid refresh token.
+
+REFRESH_TOKEN=$1
+
+RESPONSE=$(curl -H "Authorization: Bearer ${REFRESH_TOKEN}" \
+                https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/accesstoken \
+                -XPOST)
+
+if [ ! "$RESPONSE" ]; then
+    echo "Request failed"
+    exit;
+fi
+
+echo "$RESPONSE" | python -m json.tool
--- scripts/refreshtoken.sh
+++ scripts/refreshtoken.sh
@@ -1,1 +1,23 @@
-curl -H "Authorization: Bearer $1" -d redirectUri=https%3A%2F%2F127.0.0.1%3A8100 https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/refreshtoken
+#!/usr/bin/env bash
+#
+# Send a request to the /refreshtoken endpoint to obtain a refresh token and access token from
+# a spotify authorization code. To obtain the authorization code, visit the following link in a
+# browser, and extract the 'code=' url param.
+#
+# https://accounts.spotify.com/authorize?client_id=b231329aba1a4c539375436a267db917&response_type=code&redirect_uri=https://127.0.0.1:8100&scope=playlist-modify-public
+#
+# Note: client id is not a secret. Redirect uri must be https://127.0.0.1:8100 as it's whitelisted.
+#       can change the scope on the request to include playlist-modify-private for private playlists.
+
+AUTH_CODE=$1
+
+RESPONSE=$(curl -H "Authorization: Bearer ${AUTH_CODE}" \
+                -d redirectUri=https%3A%2F%2F127.0.0.1%3A8100 \
+                https://wumxvuo5nb.execute-api.us-east-1.amazonaws.com/dev/refreshtoken)
+
+if [ ! "$RESPONSE" ]; then
+    echo "Request failed"
+    exit;
+fi
+
+echo "$RESPONSE" | python -m json.tool
