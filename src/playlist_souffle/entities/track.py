1204800d35d43025cbfcb806284b8c9412a4f75c
zhammer <zachary_hammer@brown.edu>
2018-03-30 09:16:00 -0400

    Add doctests to entities.track::extract_track_collection_id

--- src/playlist_souffle/entities/track.py
+++ src/playlist_souffle/entities/track.py
@@ -2,7 +2,16 @@
 
 
 def extract_track_collection_id(track, collection_type):
-    """Extract the collection id of a track for the given collection_type. For example, if
-    collection_type is 'artist', return the id of the track's artist.
+    """Extract the collection id of a track for the given collection_type.
+
+    >>> from playlist_souffle.definitions.track import Track
+    >>> my_track = Track(id='TRACK_ID', artist='ARTIST_ID', album='ALBUM_ID')
+
+    >>> extract_track_collection_id(my_track, 'artist')
+    'ARTIST_ID'
+
+    >>> extract_track_collection_id(my_track, 'album')
+    'ALBUM_ID'
+
     """
     return track._asdict()[collection_type]
