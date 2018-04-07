990226c9d971a5b9e01dfa9ad4454c8fba92a5de
zhammer <zachary_hammer@brown.edu>
2018-04-07 10:41:42 -0400

    Improve readability of doctest
    As per PR #4

--- playlist_souffle/entities/track.py
+++ playlist_souffle/entities/track.py
@@ -5,13 +5,13 @@ def extract_track_collection_id(track, collection_type):
     """Extract the collection id of a track for the given collection_type.
 
     >>> from playlist_souffle.definitions.track import Track
-    >>> my_track = Track(id='TRACK_ID', artist='ARTIST_ID', album='ALBUM_ID')
+    >>> my_track = Track(id='TRACK_ID', artist='MY_ARTIST_ID', album='MY_ALBUM_ID')
 
     >>> extract_track_collection_id(my_track, 'artist')
-    'ARTIST_ID'
+    'MY_ARTIST_ID'
 
     >>> extract_track_collection_id(my_track, 'album')
-    'ALBUM_ID'
+    'MY_ALBUM_ID'
 
     """
     return track._asdict()[collection_type]
