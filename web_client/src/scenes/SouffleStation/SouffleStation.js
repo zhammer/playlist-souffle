a3b9e7d0c5d0815be69a6ecc80211cce4827ef91
zhammer <zachary_hammer@brown.edu>
2018-07-04 13:06:22 -0400

    [WIP] Improve SouffleStation

--- web_client/src/scenes/SouffleStation/SouffleStation.js
+++ web_client/src/scenes/SouffleStation/SouffleStation.js
@@ -7,38 +7,43 @@ import PlaylistWidget from './components/PlaylistWidget';
 // import { TileColumn, Tile } from 'components/TileGrid';
 
 const Layout = styled('div')`
-  width: 60%;
   min-width: 20em;
   max-width: 50em;
-  margin: 0 auto;
-  display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  grid-template-columns: 10% 1fr 10%;
+  grid-template-areas: ". title ." ". playlist ." "button button button";
+  grid-row-gap: 1em;
+  width: 100%;
+  margin: 0 auto;
   height: 90vh;
-
 `;
 
 // TODO: I should make an h2 or this.
 const PlaylistName = styled(StyledH3)`
+  display: block;
   text-transform: uppercase;
-  margin: 1em;
+  margin: 0;
+  padding-top: .5em;
+  grid-area: title;
+  width: 100%;
   transform: scale(1.25);
-  display: none;
-
+  overflow: hidden;
   @media (min-width: 35em) {
-    display: inherit;
   }
 `;
 
 const SouffleButton = styled(CtaButton)`
   justify-self: center;
-  margin: 1em;
-  margin-top: .5em;
+  margin: 0;
+  grid-area: button;
   transition: background .5s linear;
   text-shadow: 1px 1px ${colors.darkBlue};
-  padding-left: 2.25em;
-  padding-right: 2.25em;
+  padding-left: 1.25em;
+  padding-right: 1.25em;
+  font-size: 1.25rem;
 
   &:hover {
     background: ${colors.lightYellow};
@@ -47,16 +52,18 @@ const SouffleButton = styled(CtaButton)`
   @media (min-width: 50em) {
     padding-left: 3em;
     padding-right: 3em;
-    margin: 1em;
     font-size: 1.75rem;
   }
 `;
 
 const Playlist = styled(PlaylistWidget)`
-  margin-top: 1em;
   width: 100%;
-  flex: 1 1 70%;
+  height: 100%;
+  grid-area: playlist;
 `;
+
+const ARTIST_EMOJI = '\uD83D\uDC69\uD83C\uDFFD\u200D\uD83C\uDFA8';
+const ALBUM_EMOJI = '\uD83D\uDCBD';
 
 const SouffleStation = ({ playlist, onSouffleButtonClicked, souffledFrom }) => (
   <Layout>
@@ -65,7 +72,7 @@ const SouffleStation = ({ playlist, onSouffleButtonClicked, souffledFrom }) => (
     </PlaylistName>
     <Playlist uri={playlist && playlist.uri} />
     {playlist && <SouffleButton onClick={onSouffleButtonClicked}>
-        {souffledFrom ? 'ReSouffle' : 'Souffle'}
+        {souffledFrom ? 'ReSouffle' : 'Souffle'} by {ARTIST_EMOJI}
     </SouffleButton>}
   </Layout>
 );
