c7cfd1de8e09420a3e4ffa3ef9adde4eee71d6c6
zhammer <zachary_hammer@brown.edu>
2018-07-06 21:22:56 -0400

    Add Loading animation!

--- web_client/src/scenes/Loading/index.js
+++ web_client/src/scenes/Loading/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { StyledH3 } from 'components/headers';
+import Loading from './Loading';
+
+// import styled from 'react-emotion';
+// import { StyledH3 } from 'components/headers';
+
+// const Container = styled('div')`
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// const Loading = () => (
+//   <Container>
+//     <StyledH3>Loading</StyledH3>
+//   </Container>
+// );
+
+// export default Loading;
 
 const Container = styled('div')`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-left: 2.5em;
 `;
 
-const Loading = () => (
+const LoadingStyled = () => (
   <Container>
-    <StyledH3>Loading</StyledH3>
+    <Loading />
   </Container>
-);
+)
 
-export default Loading;
+export default LoadingStyled;
