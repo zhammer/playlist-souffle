8214441059dcc50558d266ab29c79f7ecaacdc15
zhammer <zachary_hammer@brown.edu>
2018-07-10 19:05:01 -0400

    Add documentation to selectors

--- web_client/src/selectors/index.js
+++ web_client/src/selectors/index.js
@@ -6,18 +6,27 @@ import { getAuthCode, getSouffledFromId, getPathPlaylistId, } from './router';
 
 export { getAccessToken, getFetchingAccessToken, getPlaylists, getFetchingPlaylists, getPathPlaylistId, getSouffleBy, getAuthCode };
 
+/**
+ *  Get whether or not the application is in a state of loading some data.
+ */
 export const getLoading = createSelector(
   [getFetchingAccessToken, getFetchingPlaylists, getFetchingRefreshToken, getSouffleing],
   (fetchingAccessToken, fetchingPlaylists, fetchingRefreshToken, souffleing) =>
     fetchingAccessToken || fetchingPlaylists || fetchingRefreshToken || souffleing
 );
 
+/**
+ *  Get the current playlist if there is one, otherwise return null.
+ */
 export const getCurrentPlaylist = createSelector(
   [getPlaylists, getPathPlaylistId],
   (playlists, pathPlaylistId) =>
     pathPlaylistId && playlists.find(playlist => playlist.id === pathPlaylistId)
 );
 
+/**
+ *  Get the playlist the current playlist was souffled from if there is one, otherwise return null.
+ */
 export const getSouffledFrom = createSelector(
   [getPlaylists, getSouffledFromId],
   (playlists, souffledFromId) =>
--- web_client/src/selectors/router.js
+++ web_client/src/selectors/router.js
@@ -4,21 +4,36 @@ import { createSelector } from 'reselect';
 export const getPathname = state => state.router.location.pathname;
 export const getSearch = state => state.router.location.search;
 
+/**
+ *  Get the auth code from the current routes query params (i.e. '...?code={authcode}') if it exists,
+ *  otherwise return null.
+ */
 export const getAuthCode = createSelector(
   [getSearch],
   search => queryString.parse(search).code
 );
 
+/**
+ *  Given a string and a delimiter, split the string into substrings and return the last substring.
+ *  ex: lastSubstring('hey-there-joe', '-') => 'joe'
+ */
 const lastSubstring = (string, delim) => {
   const substrings = string.split(delim);
   return substrings[substrings.length - 1];
 };
 
+/**
+ *  Get the playlist id from the current route (i.e. '.../playlists/{id}').
+ *  Note: If needed this could explicitly return null if the route is not at 'playlists/{id}'.
+ */
 export const getPathPlaylistId = createSelector(
   [getPathname],
   pathname => lastSubstring(pathname, '/')
 );
 
+/**
+ *  Get the '?souffleFrom={playlistId}' from the current route.
+ */
 export const getSouffledFromId = createSelector(
   [getSearch],
   search => queryString.parse(search).souffledFrom
