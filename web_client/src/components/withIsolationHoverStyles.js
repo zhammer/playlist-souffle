cc3f5a320a669a40b95e3d739ce3b421fc051855
zhammer <zachary_hammer@brown.edu>
2018-07-10 19:13:59 -0400

    Document withIsolationHoverStyles

--- web_client/src/components/withIsolationHoverStyles.js
+++ web_client/src/components/withIsolationHoverStyles.js
@@ -1,12 +1,24 @@
 import styled, { css } from 'react-emotion';
 
+/**
+ *  Style a parent component so that upon hovering over one of its children, that child is isolated
+ *  from the non-hovered children.
+ *
+ *  When hovering over a ComponentWithIsolationHoverStyles' children, the hovered child will
+ *  display a `childHoveredStyle` and all other elements will display a `childIgnoredStyle`,
+ *  with a transition specified by `transitionStyle`.
+ *
+ *  Styling a parent component with this function disables pointer-events on the parent component.
+ *
+ *  Inspired by Sarah Drasner's example from SVG Animations: https://codepen.io/sdras/details/qOdWEP.
+ */
 const withIsolationHoverStyles = (
   ParentComponent,
   childHoveredStyle,
   childIgnoredStyle,
   transitionStyle=css`transition: all .5s ease-out;`
-) => {
-  const StyledParentComponent = styled(ParentComponent)`
+) => (
+  styled(ParentComponent)`
     pointer-events: none;
     & > * {
       pointer-events: auto;
@@ -20,10 +32,8 @@ const withIsolationHoverStyles = (
     &:hover > *:hover {
       ${childHoveredStyle}
     }
-  `;
-
-  return StyledParentComponent;
-};
+  `
+);
 
 
 export default withIsolationHoverStyles;
